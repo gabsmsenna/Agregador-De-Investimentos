2f4a4a48f48ab91ba0d7790c89145c4030a484b8
Gabriel <gabrielmagalhaes3344@gmail.com>
2024-11-08 13:10:00 -0300

    Entidade Stock criada

---
+++ src/main/java/com/gabrielsena/agregadordeinvestimento/entity/Stock.java
@@ -0,0 +1,27 @@
+package com.gabrielsena.agregadordeinvestimento.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_stock")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Stock {
+
+    @Id
+    @Column(name = "stock_id")
+    private String stockId;
+
+    @Column(name = "description")
+    private String description;
+
+}
