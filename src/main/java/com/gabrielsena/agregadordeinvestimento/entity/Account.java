a3ffe50eea94c44614dc300d2982a305dd392dd9
Gabriel <gabrielmagalhaes3344@gmail.com>
2024-11-08 12:59:34 -0300

    Criado entidade Account

---
+++ src/main/java/com/gabrielsena/agregadordeinvestimento/entity/Account.java
@@ -0,0 +1,44 @@
+package com.gabrielsena.agregadordeinvestimento.entity;
+
+import jakarta.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_account")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "account_id")
+    private UUID accountId;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Account(UUID accountId, String description) {
+        this.accountId = accountId;
+        this.description = description;
+    }
+
+    public Account() {}
+
+    public UUID getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(UUID accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
